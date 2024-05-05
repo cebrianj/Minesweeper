56a717a73b065696eedfa6c89adf50a116744f5d
cebrianj <cebrianj@gmail.com>
2024-05-05 22:38:37 +0200

    Change emoji and spacer in board drawer

--- board_drawer.py
+++ board_drawer.py
@@ -4,25 +4,25 @@ from model.cell_value import CellValue
 
 class BoardDrawer:
     _CellValue_SymbolMap = {
-        -3: " 💣 ",
-        -2: " 🚩 ",
-        -1: " 🧱 ",
-        0: " ⬜ ",
-        1: "  1 ",
-        2: "  2 ",
-        3: "  3 ",
-        4: "  4 ",
-        5: "  5 ",
-        6: "  6 ",
-        7: "  7 ",
-        8: "  8 ",
+        -3: "💣",
+        -2: "🚩",
+        -1: "🧱",
+        0: "🔲",
+        1: " 1",
+        2: " 2",
+        3: " 3",
+        4: " 4",
+        5: " 5",
+        6: " 6",
+        7: " 7",
+        8: " 8",
     }
 
     @staticmethod
     def draw(board: Board) -> None:
         for row in board.get_representation().get_rows():
             print(
-                "".join(
+                " ".join(
                     (
                         BoardDrawer._CellValue_SymbolMap[cellValue.value]
                         for cellValue in row
